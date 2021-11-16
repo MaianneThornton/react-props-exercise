827cb5c50165782593ef8374b3ae0aff58ec80dd
MaianneThornton <maianne.thornton@gmail.com>
2021-11-16 09:53:39 -0500

    Pre Bonus Exercise completed

--- src/exercise/Exercise.js
+++ src/exercise/Exercise.js
@@ -9,11 +9,16 @@
   
 // )
 // };
-function Exercise ({good = `Awesome`, bad = `Terrible`}){
-  // 3b. Back to the Exercise.js file, use destructuring in the Exercise function component to set each prop (AKA Property) to the name of the property.
+// 3b. Back to the Exercise.js file, use destructuring in the Exercise function component to set each prop (AKA Property) to the name of the property.
+// 3d. Set the following preset values for the "good" and "bad" properties/parameters...
+// Preset value for good: "Great"
+// Preset value for bad: "Not Great"
+function Exercise ({good = `Great`, bad = `Not Great`}){
   return (
-      // 3c. In the Exercise function component, replace the text of "Great" in the first h1 with the "good" property/parameter. Also, replace the text of "Not Great" in the 2nd h1 with the "bad" property/parameter. (HINT: Remember to use brackets {}) (NOTE: If this is done correctly, then "Awesome" & "Terrible" should display in the browser)
     <div>
+      {/* // 3c. In the Exercise function component, replace the text of "Great" in the first h1 with the "good" property/parameter. Also, replace the text of "Not Great" in the 2nd h1 with the "bad" property/parameter. (HINT: Remember to use brackets {}) (NOTE: If this is done correctly, then "Awesome" & "Terrible" should display in the browser) */
+      // (NOTE: If this is done correctly, then "Great" & "Not Great" should display in the browser above "Awesome" & "Terrible")
+    }
       <h1>{good}</h1>
       <h1>{bad}</h1>
     </div>
@@ -22,14 +27,6 @@ function Exercise ({good = `Awesome`, bad = `Terrible`}){
 };
 
 
-
-
-
-
-// 3d. Set the following preset values for the "good" and "bad" properties/parameters...
-// Preset value for good: "Great"
-// Preset value for bad: "Not Great"
-// (NOTE: If this is done correctly, then "Great" & "Not Great" should display in the browser above "Awesome" & "Terrible")
 // Important: Remember to stop your React App from running after you complete the exercise. You can do this by opening the terminal session that the React App is running on and pressing (Control + C) or you can click the trash can icon to completely end that terminal session.
 // BONUS TERRITORY
 // J. Create a new folder/directory in the “src” folder called “bonus”
